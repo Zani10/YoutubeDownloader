9bcf08412d8c51355f0c258438fce95c55d26d60
Zani Dobruna <zani.dobruna@student.ehb.be>
2024-12-24 03:49:51 +0100

    ff

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
-  },
   resolve: {
     alias: {
-      '@': '/src',
-    },
+      '@': path.resolve(__dirname, './src')
+    }
   },
-  server: {
-    port: 3000
+  build: {
+    outDir: 'dist',
+    sourcemap: true
   }
 }) 
